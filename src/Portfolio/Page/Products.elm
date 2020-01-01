056b1af0ab4bdc5434d2297fee56b9041a89d1c4
tomato3713 <weasel.wt@gmail.com>
2020-01-01 18:40:09 +0900

    add about product

--- src/Portfolio/Page/Products.elm
+++ src/Portfolio/Page/Products.elm
@@ -3,6 +3,7 @@ module Portfolio.Page.Products exposing (Model, Msg, Route, page, route)
 import Browser exposing (Document, UrlRequest(..))
 import Browser.Navigation exposing (Key, load, pushUrl)
 import Html
+import Html.Attributes
 import Maybe exposing (Maybe(..))
 import Portfolio.Common
 import Portfolio.Root as Root exposing (Flags, Session)
@@ -59,6 +60,30 @@ view _ =
             []
             [ Html.text "Products" ]
         , Portfolio.Common.menu
+        , Html.div
+            [ Html.Attributes.class "contents" ]
+            [ Html.p
+                [ Html.Attributes.class "about-technology" ]
+                [ Html.text
+                    "N88互換Basic for Windows95 でシューティングゲームを作った時の感動が忘れられず、大学生となった今プログラミングだけでなくコンピュータサイエンス全般を理解すべく学習しています。"
+                ]
+            , Html.p
+                [ Html.Attributes.class "about-technology" ]
+                [ Html.text
+                    "サーバ管理・運用技術及び開発支援ツールに特に関心を持って取り組んでいます。言語については、C言語、JavaScript、Elm、Ruby等多彩な言語に触れてきたが、簡潔で協力な記述能力を持つGo言語を好んで使用しています。個人での製作の他に、チームでの開発経験があります。"
+                ]
+            , Html.p
+                [ Html.Attributes.class "about-technology" ]
+                [ Html.text
+                    "チームとしてだけでなく、個人としても社会に貢献できるように高い技術を持つことが第一目標です。"
+                ]
+            , Html.div
+                [ Html.Attributes.class "products-content" ]
+                [ Html.h1
+                    []
+                    []
+                ]
+            ]
         , Portfolio.Common.linklist
         ]
     }
